12240986a149de021900d2e1dca9d0eee35bbd2b
Abhijeet Mohanan <55363615+InfernoVirus@users.noreply.github.com>
2020-11-09 11:09:34 +0530

    Update nema23run.py

--- nema23run.py
+++ nema23run.py
@@ -1,3 +1,6 @@
+#17 -- enable pin  || enable pin is active low
+#27 -- dir +
+#22 -- pul +
 import RPi.GPIO as GPIO
 import time
 a=0
@@ -8,11 +11,11 @@ GPIO.setup(22,GPIO.OUT)
 try:
     GPIO.output(17, GPIO.LOW) #enable high
     GPIO.output(27, GPIO.HIGH) #dir enable
-    GPIO.output(22, GPIO.LOW) #dir enable
+    GPIO.output(22, GPIO.LOW) #pulse low
     while True:
         time.sleep(1)
-        #val=int(input())
-        val=1
+        #val=int(input()) # uncomment this section to control the 72 degree movment || pass 1 as input 
+        val=1   # comment this value if uncommenting the above one
         if val==1:
             for i in range(0,640): 
                 GPIO.output(22, GPIO.HIGH)
